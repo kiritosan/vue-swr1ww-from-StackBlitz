f19a39cfcd883c867efaab2134d2cacadc8d0d7a
kiritosan <1121567132@qq.com>
2021-03-18 18:52:51 +0800

    2 files modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <cell />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Cell from ./Cell.vue
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  component: {
+    Cell
   }
-}
+
+} 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/Cell.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="true">{{text}}</div>
+  <div v-else></div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {text:'X'}
+    }
+}
+</script>
+
+<style>
+  #app {
+    
+    text-align: center;
+    border:1px;
+
+  }
+</style>
